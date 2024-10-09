4827fac0844d3886f646657e0de6d43e77c7e3db
Phelipe Cezario <phelipesamuel123@gmail.com>
2024-10-08 23:44:44 -0300

    feat: Connecting our DB to the API

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-   console.log("SERVER is running"); 
-})
+mongoose.connect(process.env.MONGODB_URI, {
+   dbName: "quebra-molasdb",
+}).then(() => console.log("Connected to MongoDB"))
+   .catch((err) => console.error("Failed to connect to MongoDB", err));
+
+app.listen(process.env.PORT || 3000, () => console.log("SERVER is running"));
