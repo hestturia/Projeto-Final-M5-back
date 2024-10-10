492dfcfcc31d25ac85493f86703fa03dc674917d
Phelipe Cezario <phelipesamuel123@gmail.com>
2024-10-09 23:40:32 -0300

    feat: Sequelize DB configuration

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import trafficRoutes from './routes/traffic.routes.js';
 import corsConfig from "./config/cors.config.js";
+import { testDatabase } from '../database/databaseConfig.js';
 
 dotenv.config();
 
@@ -11,12 +12,14 @@ const app = express();
 app.use(express.json());
 app.use(corsConfig);
 
+testDatabase()
+
 mongoose.connect(process.env.MONGODB_URI, {
    dbName: "test", 
  }).then(() => console.log("Connected to MongoDB"))
    .catch((err) => console.error("Failed to connect to MongoDB", err));
  
 
-app.use('/api', trafficRoutes);
+app.use('/', trafficRoutes);
 
 app.listen(process.env.PORT || 3000, () => console.log("SERVER is running"));
