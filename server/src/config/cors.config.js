c3e5fd2295a0a2752915f2570434d19fa144576e
Samuel-Kepler <seu_email@example.com>
2024-10-09 21:40:59 -0300

    build: Config cors

---
+++ server/src/config/cors.config.js
@@ -0,0 +1,10 @@
+import cors from 'cors';
+
+const corsConfig = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+export default cors(corsConfig) 
